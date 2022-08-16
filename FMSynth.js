9ae872ac21f694aed5ad473c10de9d1dea72af5b
petethomasmusique <petethomasmusique@gmail.com>
2022-08-16 21:07:53 +0100

    _n

--- FMSynth.js
+++ FMSynth.js
@@ -1,78 +1,45 @@
 import { mtf, formatOscType } from "./utils/core";
-import { formatCurve, timeToEvent } from "./utils/tone";
-import { Oscillator, AmplitudeEnvelope, Multiply, Signal, Add } from "tone";
+import { formatCurve } from "./utils/tone";
+import { FMSynth } from "tone";
 import BaseSynth from "./BaseSynth";
 
 // TODO: _n()
+// TODO: presets
 
-class FMSynth extends BaseSynth {
-    #carrierWaveform
-    #modulatorWaveform
+class FMSynth2 extends BaseSynth {    
+    synth;
     
-    constructor(options = {carr: 'sine', mod: 'sine'}) {
+    constructor() {
         super()
-
-        const { carr, mod } = options
-        this.#carrierWaveform = formatOscType(carr)
-        this.#modulatorWaveform = formatOscType(mod)
-
         this.#initGraph()
     }
 
     #initGraph() {
-        // create carrier oscillator
-        this.carrier = new Oscillator(220, this.#carrierWaveform).connect(this.envelope).start()
-        
-        // create modulator oscillator and envelope
-        this.modulatorEnvelope = new AmplitudeEnvelope({attack: 0, decay: 0, sustain: 1, release: 1})
-        this.modulator = new Oscillator(220, this.#modulatorWaveform).connect(this.modulatorEnvelope).start()
-        
-        // create signals to handle carr and mod frequencies
-        this.carrierFrequency = new Signal(220, 'frequency')
-        this.modulationFrequency = new Add().connect(this.carrier.frequency)
-        
-        // control harmonicity by multiplying the carrier frequency by ratio
-        this.harmonicity = new Multiply(1).connect(this.modulator.frequency)
-        this.carrierFrequency.connect(this.harmonicity)
-        this.harmonicityRatio = new Signal(1.5).connect(this.harmonicity.factor)
-        
-        // multiply carrier frequency by the modulation envelope
-        this.modulation = new Multiply()
-        this.modulatorEnvelope.connect(this.modulation)
-        this.carrierFrequency.connect(this.modulation.factor)
-        
-        // control modulation index my multiplying modulation by an index
-        this.modulationIndex = new Multiply()
-        this.modulation.connect(this.modulationIndex)
-        this.modulationGain = new Signal(1).connect(this.modulationIndex.factor)
-
-        // add carrier frequency signal to modulation index to get modulation frequency
-        this.carrierFrequency.connect(this.modulationFrequency)
-        this.modulationIndex.connect(this.modulationFrequency.addend)
+        this.synth = new FMSynth()
+        this.envelope.dispose() // not needed
+        this.envelope = this.synth.envelope
+        this.synth.connect(this.panner)
     }
 
     play(params = {}, time) {
         this.time = time
         this.setParams(params)
-        this.note = params.n || 220
         
-        this.envelope.triggerAttackRelease(this.dur, time, this.amp)
-        this.modulatorEnvelope.triggerAttackRelease(this.dur, time, this.amp)
+        this.synth.triggerAttackRelease(mtf(params.n) || 220, this.dur, time, this.amp * 1.25)
         
-        this.disposeTime = time + this.dur + this.envelope.release + 0.1
+        this.disposeTime = time + this.dur + this.synth.envelope.release + 0.1
         this.dispose(this.disposeTime)
     }
 
-    set note(value) { this.carrierFrequency.setValueAtTime(mtf(value), this.time) }
 
-    set moda(value) { this.modulatorEnvelope.attack = value }
-    set modd(value) { this.modulatorEnvelope.decay = value }
-    set mods(value) { this.modulatorEnvelope.sustain = value }
-    set modr(value) { this.modulatorEnvelope.release = value }
+    set moda(value) { this.synth.modulationEnvelope.attack = value }
+    set modd(value) { this.synth.modulationEnvelope.decay = value }
+    set mods(value) { this.synth.modulationEnvelope.sustain = value }
+    set modr(value) { this.synth.modulationEnvelope.release = value }
 
-    set modacurve(value) { this.modulatorEnvelope.attackCurve = formatCurve(value) }
-    set moddcurve(value) { this.modulatorEnvelope.decayCurve = formatCurve(value) }
-    set modrcurve(value) { this.modulatorEnvelope.releaseCurve = formatCurve(value) }    
+    set modacurve(value) { this.synth.modulationEnvelope.attackCurve = formatCurve(value) }
+    set moddcurve(value) { this.synth.modulationEnvelope.decayCurve = formatCurve(value) }
+    set modrcurve(value) { this.synth.modulationEnvelope.releaseCurve = formatCurve(value) }    
     
     set modcurve(value) { 
         this.modacurve = formatCurve(value)
@@ -80,16 +47,19 @@ class FMSynth extends BaseSynth {
         this.modrcurve = formatCurve(value)
     }
 
-    set harm(value) { this.harmonicity.setValueAtTime(value, this.time) }
-    set modi(value) { this.modulationIndex.setValueAtTime(value, this.time) }
+    set harm(value) { this.synth.harmonicity.setValueAtTime(value, this.time) }
+    set modi(value) { this.synth.modulationIndex.setValueAtTime(value, this.time) }
 
-    _harm(value, time, lag = 0.1) { this.harmonicityRatio.rampTo(value, lag, time) }
+    
+    _n(value, time, lag = 0.1) { this.synth.frequency.rampTo(value, lag, time) }
+    _n = this._n.bind(this)
+
+    _harm(value, time, lag = 0.1) { this.synth.harmonicity.rampTo(value, lag, time) }
     _harm = this._harm.bind(this)
 
-    _modi(value, time, lag = 0.1) { this.modulationIndex.rampTo(value, lag, time) }
+    _modi(value, time, lag = 0.1) { this.synth.modulationIndex.rampTo(value, lag, time) }
     _modi = this._modi.bind(this)
-
 }
 
 
-export default FMSynth
+export default FMSynth2
--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 import FMSynth from "./FMSynth";
-import FMSynth2 from "./FMSynth2";
 
 export const CtFMSynth = FMSynth
-export const CtFMSynth2 = FMSynth2
