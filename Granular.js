988f12408ca6add14b00bb2cf7f0f7f8916f6f6b
petethomasmusique <petethomasmusique@gmail.com>
2022-08-17 21:23:40 +0100

    grranular trigger and envelope

---
+++ Granular.js
@@ -0,0 +1,41 @@
+import { GrainPlayer, context, Transport } from "tone";
+import BaseSynth from "./BaseSynth";
+
+// TODO: presets
+class Granular extends BaseSynth {    
+    synth;
+    #buffer
+    #begin = 0
+    
+    constructor(buffer) {
+        super()
+        this.#buffer = buffer
+        this.#initGraph()
+    }
+
+    #initGraph() {
+        this.synth = new GrainPlayer({loop: true, grainSize: 0.1, overlap: 0.05})
+        this.synth.buffer.set(this.#buffer)
+        this.synth.connect(this.envelope)
+        this.envelope.set({attack: 0.01, decay: 0, sustain: 1, release: 0.1})
+    }
+
+    play(params = {}, time) {
+        this.time = time
+        this.setParams(params)
+        const duration = params.dur || this.dur
+        this.synth.start(this.time, this.#begin, duration)
+        this.envelope.triggerAttackRelease(duration - this.envelope.release, this.time, this.amp)
+        
+        this.disposeTime = time + duration + 0.1
+
+        this.dispose(this.disposeTime)
+    }
+
+    cut(time) {
+        // this.disposeTime = time + this.synth.fadeOut + 1
+        // this.dispose(this.disposeTime)
+    }
+}
+
+export default Granular
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import FMSynth from "./FMSynth";
 import Sampler from "./Sampler";
+import Granular from "./Granular";
 
 export const CtFMSynth = FMSynth
 export const CtSampler = Sampler
+export const CtGranular = Granular
