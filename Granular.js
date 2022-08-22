b5cb8e920b5d6650ee650f82555b68d42e5d2050
petethomasmusique <petethomasmusique@gmail.com>
2022-08-22 21:51:19 +0100

    working granular

--- Granular.js
+++ Granular.js
@@ -2,7 +2,9 @@ import { GrainPlayer, context, Signal, Clock, Transport } from "tone";
 import BaseSynth from "./BaseSynth";
 
 const { bpm } = Transport;
+
 // TODO: presets
+// TODO: snap
 class Granular extends BaseSynth {    
     synth;
     #n = 60
@@ -20,14 +22,14 @@ class Granular extends BaseSynth {
     #initGraph(buffer) {
         this.synth = new GrainPlayer({loop: true, grainSize: 0.1, overlap: 0.05, url: buffer}).start()
         this.synth.connect(this.envelope)
-        this.envelope.set({attack: 0.01, decay: 0, sustain: 1, release: 0.1})
+        this.envelope.set({attack: 0.1, decay: 0, sustain: 1, release: 0.1})
         this.rateRamp = new Signal(this.synth.playbackRate, 'number')
         this.pitchRamp = new Signal(0, 'number')
         this.clock = new Clock(time => {
             // set playbackRate
-            this.synth.playbackRate !== this.rateRamp.value && this.synth.set({playbackRate: this.rateRamp.getValueAtTime(time)});
+            this.synth.set({playbackRate: this.rateRamp.getValueAtTime(time)});
             // set detune value (cents)
-            this.synth.detune !== this.pitchRamp.value && this.synth.set({detune: this.pitchRamp.getValueAtTime(time)});
+            this.synth.set({detune: this.pitchRamp.getValueAtTime(time)});
         }, 48).start();
     }
 
@@ -78,9 +80,10 @@ class Granular extends BaseSynth {
     _rate = this._rate.bind(this)
     
     // grain pitch - assumes note 60 is original speed of sample
-    _n(value, time, lag = 0.1) { 
+    _n(value, time, lag = 0.01) {
+        console.log(lag/10)
         this.pitchRamp.cancelScheduledValues(time)
-        this.pitchRamp.setValueAtTime(Math.floor((value - 60) * 100), lag, time)
+        this.pitchRamp.rampTo(Math.floor((value - 60) * 100), lag/10, time)
     }
     _n = this._n.bind(this)
 }
