c49cad7c45cc5fe20e4eb462bf70605c8fc7e2f7
petethomasmusique <petethomasmusique@gmail.com>
2022-11-28 21:04:29 +0000

    cutoff and drift for filter synth

--- rnbo-synths/FilterSynth.js
+++ rnbo-synths/FilterSynth.js
@@ -1,9 +1,8 @@
 import BaseSynth from './BaseSynth';
-// todo: cutoff
 // limiter?
 class FilterSynth extends BaseSynth {
     oscTypes = ['sine', 'saw', 'tri', 'pulse', 'noise']
-    json = new URL('./json/filter-synth.export.json', import.meta.url)
+    json = new URL('./json/filter-synth2.export.json', import.meta.url)
     
     constructor() {
         super()
@@ -18,12 +17,14 @@ class FilterSynth extends BaseSynth {
     set osc(type) { this.setDeviceParams('osc', this.oscTypes.indexOf(type) || 0) }
     set res(value) { this.setDeviceParams('res', value) }
     set cutoff(value) { this.setDeviceParams('cutoff', value) }
+    set drift(value) { this.setDeviceParams('drift', value) }
 
     /*
      * Mutable params
     */
     _res(value, lag = 0.1) { this.mutateParam('res', value, lag)}
     _cutoff(value, lag = 0.1) { this.mutateParam('cutoff', value, lag)}
+    _drift(value, lag = 0.1) { this.mutateParam('drift', value, lag)}
 }
 
 export default FilterSynth
