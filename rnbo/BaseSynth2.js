50ba88a47110ba7fc9803db964ca30755ffd62ae
petethomasmusique <petethomasmusique@gmail.com>
2022-12-05 19:47:13 +0000

    working synth

--- rnbo/BaseSynth2.js
+++ rnbo/BaseSynth2.js
@@ -1,6 +1,7 @@
 import { context as toneContext, Gain } from 'tone';
 import { dummy } from './utils';
 import { createDevice, MIDIEvent, MessageEvent } from '@rnbo/js'
+import { beatsToSeconds } from "../utils/tone";
 
 const context = toneContext.rawContext._nativeAudioContext || toneContext.rawContext._context;
 
@@ -9,7 +10,7 @@ class BaseSynth {
     defaults = {}
     device = null
     ready = false
-    params = ['n', 'pan', 'vol']
+    params = ['n', 'pan', 'vol', 'a', 'd', 's', 'r', 'cutoff']
 
     constructor() {
         this.output = new Gain(1);
@@ -26,6 +27,11 @@ class BaseSynth {
         this.ready = true
     }  
 
+    initParams() {
+        this.params.forEach(key => this[key] = (value, time) => this.messageDevice(key, value, time))
+        Object.keys(this.settable).forEach(key => this[key] = this[key].bind(this))
+    }
+
     messageDevice(tag, payload, time) {
         const message = new MessageEvent((time * 1000) - 10, tag, [ payload ]);
         this.device.scheduleEvent(message);
@@ -36,20 +42,16 @@ class BaseSynth {
         this.output.connect(node)
     }
 
-    bindProps() {
-        const props = {...this.settable, ...this.mutable}
-        Object.keys(props).forEach(key => this[key] = this[key].bind(this))
-    }
-
     get settable() { 
         return this.params.reduce((obj, key) => ({ ...obj, [key]: this[key] }), {})
     }
 
     setParams(params, time) {
         const settable = this.settable
-        Object.entries(params).forEach(([key, value]) => {
-            settable[key] && settable[key](value, time)
-        })
+        Object.entries(params)
+            .forEach(([key, value]) => {
+                settable[key] && settable[key](value, time)
+            })
     }
 
     play(params = {}, time) {
@@ -60,7 +62,7 @@ class BaseSynth {
         const {n, dur, amp} = params
 
         const noteOnEvent = new MIDIEvent(time * 1000, 0, [144, n, 66 * (amp || 1)]);
-        const noteOffEvent = new MIDIEvent((time + dur) * 1000, 0, [128, n, 0]);
+        const noteOffEvent = new MIDIEvent((time + beatsToSeconds(dur)) * 1000, 0, [128, n, 0]);
 
         this.device.scheduleEvent(noteOnEvent);
         this.device.scheduleEvent(noteOffEvent)
@@ -74,21 +76,9 @@ class BaseSynth {
     mutate(params = {}, time, lag = 0.1) {
         if(!this.ready) return
         this.setParams(params, time)
+        // TODO: check this...
         this.messageDevice('mutate', lag * 1000, time + 0.01)
     }
-
-    n(value, time) {
-        this.messageDevice('n', value, time)
-    }
-
-    vol(value, time) {
-        this.messageDevice('vol', value, time)
-    }
-
-    pan(value, time) {
-        this.messageDevice('pan', (value + 1)/2, time)
-    }
-
 }
 
 export default BaseSynth
--- rnbo/Synth2.js
+++ rnbo/Synth2.js
@@ -2,11 +2,12 @@ import BaseSynth from "./BaseSynth2";
 
 class Synth2 extends BaseSynth {
     json = new URL('./json/synth.export.json', import.meta.url)
+    params = [...this.params, 'osc', 'res', 'cutoff']
 
     constructor() {
         super()
         this.initDevice()
-        this.bindProps()
+        this.initParams()
     }
 
 }
