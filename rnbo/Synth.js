8bc718d039390655d9f233ba0afa2e3c0cc898e5
petethomasmusique <petethomasmusique@gmail.com>
2022-12-15 20:35:55 +0000

    rnbo-rewind: add defaults

--- rnbo/Synth.js
+++ rnbo/Synth.js
@@ -1,7 +1,9 @@
 import BaseSynth from "./BaseSynth";
+
 class Synth extends BaseSynth {
     json = new URL('./json/synth.export.json', import.meta.url)
     params = [...this.params, 'osc', 'res', 'cutoff', 'drift']
+    defaults = { ...this.defaults, osc: 0, res: 0, cutoff: 20000, drift: 0 }
 
     constructor() {
         super()
