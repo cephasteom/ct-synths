797f3f80ebf4837e6ad592a7a48794b75a1dbeb4
petethomasmusique <petethomasmusique@gmail.com>
2022-12-15 21:20:58 +0000

    develop: suppress older synths

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 import { immediate } from "tone"
 
-import FMSynth from "./tone-synths/FMSynth";
+// import FMSynth from "./tone-synths/FMSynth";
 // import Sampler from "./tone-synths/Sampler";
-import Granular from "./tone-synths/Granular";
-import DrumSynth from "./tone-synths/DrumSynth";
-import KarplusSynth from "./tone-synths/KarplusSynth";
-import DuoSynth from "./tone-synths/DuoSynth";
-import MonoSynth from "./tone-synths/MonoSynth";
-import MetalSynth from "./tone-synths/MetalSynth";
-import FlexSynth from "./tone-synths/FlexSynth";
+// import Granular from "./tone-synths/Granular";
+// import DrumSynth from "./tone-synths/DrumSynth";
+// import KarplusSynth from "./tone-synths/KarplusSynth";
+// import DuoSynth from "./tone-synths/DuoSynth";
+// import MonoSynth from "./tone-synths/MonoSynth";
+// import MetalSynth from "./tone-synths/MetalSynth";
+// import FlexSynth from "./tone-synths/FlexSynth";
 import Synth from "./rnbo/Synth";
 import Sampler from "./rnbo/Sampler";
 import Reverb from "./rnbo/Reverb";
 
-export const CtFMSynth = FMSynth
+// export const CtFMSynth = FMSynth
 // export const CtSampler = Sampler
-export const CtGranular = Granular
-export const CtDrumSynth = DrumSynth
-export const CtKarplusSynth = KarplusSynth
-export const CtDuoSynth = DuoSynth
-export const CtMonoSynth = MonoSynth
-export const CtMetalSynth = MetalSynth
-export const CtFlexSynth = FlexSynth
+// export const CtGranular = Granular
+// export const CtDrumSynth = DrumSynth
+// export const CtKarplusSynth = KarplusSynth
+// export const CtDuoSynth = DuoSynth
+// export const CtMonoSynth = MonoSynth
+// export const CtMetalSynth = MetalSynth
+// export const CtFlexSynth = FlexSynth
 export const CtSynth = Synth
 export const CtSampler = Sampler
 export const CtReverb = Reverb
 
 const synthMap = {
-    drum: DrumSynth, 
-    duo: DuoSynth,
-    flex: FlexSynth,
-    fm: FMSynth,
-    granular: Granular,
-    metal: MetalSynth,
-    mono: MonoSynth,
+    // drum: DrumSynth, 
+    // duo: DuoSynth,
+    // flex: FlexSynth,
+    // fm: FMSynth,
+    // granular: Granular,
+    // metal: MetalSynth,
+    // mono: MonoSynth,
     // needs to worked differently with rnbo synths
     // sampler: Sampler,
     // synth: FilterSynth
