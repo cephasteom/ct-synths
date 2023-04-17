55712f872a8066955251a8e560fc4a9d9099d404
petethomasmusique <petethomasmusique@gmail.com>
2023-04-17 21:14:04 +0100

    enable synth-lite

--- rnbo/Synth.js
+++ rnbo/Synth.js
@@ -2,12 +2,15 @@ import BaseSynth from "./BaseSynth";
 import { synthParams } from "./data";
 
 class Synth extends BaseSynth {
-    json = new URL('./json/synth.export.json', import.meta.url)
+    
     params = [...this.params, ...synthParams]
     defaults = { ...this.defaults, osc: 0, drift: 0 }
 
-    constructor() {
+    constructor(args = {}) {
         super()
+        this.json = args.lite 
+            ? new URL('./json/synth-lite.export.json', import.meta.url)
+            : new URL('./json/synth.export.json', import.meta.url)
         this.params = [...this.params, ...this.params.map(p => `_${p}`)]
         this.initDevice()
         this.initParams()
