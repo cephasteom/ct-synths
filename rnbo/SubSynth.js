2590213c0ff6427bbb0211ebe62edf64fe9ed16f
petethomasmusique <petethomasmusique@gmail.com>
2023-05-24 21:43:00 +0100

    update subsynth

--- rnbo/SubSynth.js
+++ rnbo/SubSynth.js
@@ -1,3 +1,4 @@
+import { MIDIEvent } from '@rnbo/js'
 import BaseSynth from "./BaseSynth";
 import { subParams } from "./data";
 
@@ -8,7 +9,7 @@ class SubSynth extends BaseSynth {
         ...this.defaults, 
         slide: 10, fat: 0.5, 
         dur: 1000, a: 100, d: 100, s: 0.75, r: 1000, fila: 0, fild: 100, fils: 1, filr: 100, res: 0,
-        moda: 0, modd: 0
+        moda: 10, modd: 0
     }
 
     constructor() {
@@ -16,6 +17,19 @@ class SubSynth extends BaseSynth {
         this.params = [...this.params, ...this.params.map(p => `_${p}`)]
         this.initDevice()
         this.initParams()
+    }
+
+    play(params = {}, time) {
+        
+        if(!this.ready) return
+
+        const ps = {...this.defaults, ...params }
+        const { n, amp } = ps
+
+        this.setParams(ps, time, 0)
+        
+        const noteOnEvent = new MIDIEvent(time * 1000, 0, [144, (n || 60), amp * 127]);
+        this.device.scheduleEvent(noteOnEvent);
     }
 
 }
